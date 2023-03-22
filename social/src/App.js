4829ac6bffb169649db1f43418cb0039b9e9f4a4
omersz <omer.sapmaz45@gmail.com>
2023-03-22 15:49:17 +0300

    social kısmında ufak ilerleme

--- social/src/App.js
+++ social/src/App.js
@@ -2,9 +2,22 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Profile from "./pages/profile/Profile";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 function App() {
-  return <Profile/>
+  return (
+    <Router>
+      <Routes>
+      <Route path="/"  element={ <Home/> }/>
+      <Route path="/login"  element={ <Login/> }/>
+      <Route path="/register"  element={ <Register/> }/>
+      <Route path="/profile/:username"  element={ <Profile/> }/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
