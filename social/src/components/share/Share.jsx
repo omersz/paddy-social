110eab862b035f32ef3ccfb5875a9705899990ac
omersz <omer.sapmaz45@gmail.com>
2023-04-24 11:55:55 +0300

    24.04

--- social/src/components/share/Share.jsx
+++ social/src/components/share/Share.jsx
@@ -19,10 +19,10 @@ export default function Share() {
       userId: user._id,
       desc: desc.current.value
     }
+    
     try{
       await axios.post("/posts", newPost)
     }catch(err){
-
     }
   }
 
